310c4d0e317fec64346e7d510b7e284834a55e27
Ajay Mishra <asap2656@gmail.com>
2024-03-16 23:40:03 +0530

    updated

---
+++ DAYS/DAY1/main.py
@@ -0,0 +1,33 @@
+# Write your code below this line 👇
+print('hello world')
+
+#  string
+print("this is lesson 1")
+print("this is 'python' tutorial")
+print(' this is "python" tutorial')
+# to acknowledge the written content as a string, we can use the escape character \"
+print("hello coder this is day-1 of \"python\" tutorial")
+
+# to print multiple lines in a single print statement, we can use \n
+print("hello coder\nthis is day-1 of \"python\" tutorial")
+
+# string concatenation
+print("hello" + " developers")
+
+print("java" + " " + "tutorials")
+
+#IndentationError: In python we should write the code from beginning of the line else it will throw you one indentation Error if you some space at the beginning of the code
+#__print('don\'t give any space')
+
+# code intelligence: make sure it is enabled. Code intelligence gives you autocomplete, as well as hints as you type.
+
+# input function: Takes the input from the user. In terminal window while using input function, it will wait for one prompt from the user to execute it. And also the input code line is replaced by the prompt message.
+# input('Enter your name')
+# input("Hello, " + input("Enter your name"))
+
+# len() function: It is used to get the length of the string.
+# print(len(input("Enter your name")))
+
+# variables in python: It is used to store the data.Hence we can use the variable to refer the data.
+# data = input("Enter your name: ")
+# print(data)
---
+++ DAYS/DAY2/main.py
@@ -0,0 +1,39 @@
+#Data Types in python
+
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+# string: String are the sequence of characters.String are iterable.Strings in python are surrounded by either single quotation marks, or double quotation marks.
+
+print("Hello"[0])
+
+data = "welcome to the programming world"
+data2 = 'welcome to python tutorials'
+print(data)
+print(data2)
+
+#mutliline string: Print the string in multiple lines as specified.
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+print('123_456_789' + '1000_00')
+
+# Python Numbers
+# There are three numeric types in Python:int, float, complex
+
+# Integer: Integers are whole numbers, positive or negative, without decimals, of unlimited length.
+print(123_456_789 + 1000_00)
+
+# Float: Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+print(3.14159)
+
+# Boolean: Boolean values are either True or False.
