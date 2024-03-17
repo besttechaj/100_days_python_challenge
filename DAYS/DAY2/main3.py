36296a946d5459ecb989f3c4a65b881cdc230c6d
Ajay Mishra <asap2656@gmail.com>
2024-03-17 23:29:17 +0530

    python arithmetic operators

---
+++ DAYS/DAY2/main3.py
@@ -0,0 +1,29 @@
+# python arithmetic operators
+'''
+# Priority for arithmetic operations: known as PEMDASLR (parenthesis > exponential > multi or division based on LEFT TO RIGHT, WHICHEVER COMES FIRST > ADDITION > SUBTRACTION)
+parenthesis: ()
+exponents: base ** exponents
+multiplication: *
+division: /
+addition: +
+subtraction: -
+
+'''
+
+# addition 
+print(2+4)
+
+# subtraction
+print(10-8)
+
+# multiplication
+print(6*7)
+
+# division: Return type of division is "float"
+print(6/3)
+
+# power: base ** exponential
+print(3**2)
+
+# applying PEMDASLR RULE FOR BELOW CODE
+print(3 * (3 + 3) /  3 - 3); # 3.0
---
+++ importantTools.md
@@ -0,0 +1,3 @@
+# python code debug: Thonny ( download and install it to use )
+
+## replit: It is an IDE to run your code
