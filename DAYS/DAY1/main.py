d8b77d342c43fa64c01026ae4c7a9fd97e3ba41a
Ajay Mishra <asap2656@gmail.com>
2025-03-04 13:04:53 +0530

    notes based on strings in python

--- DAYS/DAY1/main.py
+++ DAYS/DAY1/main.py
@@ -1,31 +1,53 @@
 # Write your code below this line 👇
 print('hello world')
 
-#  string
+#?  string
 print("this is lesson 1")
 print("this is 'python' tutorial")
 print(' this is "python" tutorial')
+# Using Triple Quotes (""" or ''') :for Multi-line Strings
+print("""triple quotes example""")
+print('''triple quotes example''')
 
+#? escape character
 # to acknowledge the written content as a string, we can use the escape character \"
 print("hello coder this is day-1 of \"python\" tutorial")
 
-# to print multiple lines in a single print statement, we can use \n
-print("hello coder\n this is day-1 of \"python\" tutorial")
+#? to print multiple lines in a single print statement, we can use \n (\n is a newline character, which moves the text after it to the next line.)
+print("hello developers......\n this is day-1 of \"python\" tutorial")
 
 # string concatenation
-print("shri" + " ram")
+print("introvert:" + " shy")
+print("extrovert:" + " " + "sociable")
 
-print("hare" + " " + "krishna")
+#? multi-line string in python
+# way-01
+print("""hare krishna hare krishna
+krishna krishna hare hare""")
 
-# IndentationError: In python we should write the code from beginning of the line else it will throw you one indentation Error if you give some space before the beginning of the code
+# way-02 : using \n is a newline character, backslash
+print("hare rama hare rama\n rama rama hare hare")
+
+#? IndentationError: In python we should write the code from beginning of the line else it will throw you one indentation Error if you give some space before the beginning of the code
 #__print('don\'t give any space')
 print('don\'t give any space')
 
-# code intelligence: make sure it is enabled. Code intelligence gives you autocomplete, as well as hints as you type.
+## code intelligence: make sure it is enabled. Code intelligence gives you autocomplete, as well as hints as you type.
 
-# input function: Takes the input from the user. In terminal window while using input function, it will wait for one prompt from the user to execute it. And also the input code line is replaced by the prompt message.
+#? input function: The input() function is used to take user input in Python. It always returns the input as a string unless explicitly converted to another type.
 # input('Enter your name')
 # input("Hello, " + input("Enter your name"))
+
+#? Multiple Inputs in One Line: Use split() to take multiple inputs at once.
+x, y = input("Enter two numbers: ").split()
+x = int(x)
+y = int(y)
+print("Sum:", x + y)
+
+#? Using eval() for Direct Conversion: Instead of int() or float(), you can use eval() to automatically convert numbers.
+x = eval(input("Enter a number: "))
+print("You entered:", x)
+
 
 # len() function: It is used to get the length of the string.
 # print(len(input("Enter your name")))
